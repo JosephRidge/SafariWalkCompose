c8909271a671952213ecf3eaf15b83c0f7902af8
JosephRidge <josephridge18@gmail.com>
2026-04-13 15:40:20 +0300

    update

--- app/src/main/java/com/jayr/safariwalk/ui/screens/authentication/login/LoginScreen.kt
+++ app/src/main/java/com/jayr/safariwalk/ui/screens/authentication/login/LoginScreen.kt
@@ -1,34 +1,99 @@
 package com.jayr.safariwalk.ui.screens.authentication.login
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.jayr.safariwalk.R
-import com.jayr.safariwalk.ui.components.LottieAnimationWidget
+import com.jayr.safariwalk.ui.theme.primaryColor
+import com.jayr.safariwalk.ui.theme.secondaryColor
 
 @Composable
 fun LoginScreen() {
     var emailInput by remember { mutableStateOf(TextFieldValue("")) }
     var passwordInput by remember { mutableStateOf(TextFieldValue("")) }
-    Column {
-        LottieAnimationWidget(R.raw.auth_login)
 
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+//        lottie animation
+        LottieAnimationWidget(R.raw.auth_login, 250.dp)
+//        welcome message
+        Text(
+            text = "Join SafariWalk!",
+            style = TextStyle(
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            ))
+//        email input
+        OutlinedTextField(
+            value = emailInput,
+            onValueChange={ emailInput = it },
+            label = { Text(text="Enter Email") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Email,
+                    contentDescription = "Email",
+                    tint = secondaryColor
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            shape = RoundedCornerShape(32.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = secondaryColor,
+                unfocusedBorderColor = primaryColor
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+//        password input
+//        button
+//        text button
+    }
+
+}
+
+@Composable
+fun LottieAnimationWidget(drawable: Int, size:Dp) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(drawable))
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = LottieConstants.IterateForever
+    )
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+        modifier = Modifier.size(size)
+    )
 }
 
 
