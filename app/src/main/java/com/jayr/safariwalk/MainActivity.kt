d8aacd5716c1028c38f57c533845d6252072185e
JosephRidge <josephridge18@gmail.com>
2026-04-20 14:49:45 +0300

    update

--- app/src/main/java/com/jayr/safariwalk/MainActivity.kt
+++ app/src/main/java/com/jayr/safariwalk/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jayr.safariwalk.ui.screens.authentication.login.LoginScreen
 import com.jayr.safariwalk.ui.screens.onboarding.OnboardingScreen
 import com.jayr.safariwalk.ui.theme.SafariWalkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
